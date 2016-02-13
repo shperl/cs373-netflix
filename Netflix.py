29f2210495a799073acada4d97220386c1142ce5
Stephen Perl <shp425@cs.utexas.edu>
2016-02-13 14:32:44 -0600

    added basic rmse method. next step to parse input file

--- Netflix.py
+++ Netflix.py
@@ -6,56 +6,105 @@
 # Glenn P. Downing
 # ---------------------------
 
+# -------
+# imports
+# -------
+
+from json           import load
+from pprint         import pprint
+from math           import sqrt
+from collections    import defaultdict
+
+with open('/u/fares/public_html/netflix-tests/ckc735-movies.json') as data_file:    
+    movie_averages = json.load(data_file)
+
+
 # ------------
-# Netflix_read
+# netflix_rmse
 # ------------
 
-def Netflix_read (s) :
+def netflix_rmse (actual, prediction) :
+    """
+    compute the root mean squared error between actual scores and predicted scores
+    a a list
+    p a list
+    return a decimal, representing the root mean squared error of predicted scores
+    """
+
+    # zip two lists into a list of tuples with corresponding values
+    z = zip(actual, prediction)
+    # the squared error
+    se = 0
+
+    for x, y in z:
+        d = x - y
+        ds = d**2
+        se += ds
+
+    return sqrt(se / len(z))
+
+# ------------
+# get_mse
+# ------------
+
+def netflix_rmse (actual, prediction) :
+    """
+    compute the root mean squared error between actual scores and predicted scores
+    a a list
+    p a list
+    return a decimal, representing the root mean squared error of predicted scores
+    """     
+
+
+# ------------
+# netflix_read
+# ------------
+
+def netflix_read (s) :
     """
     read two ints
     s a string
     return a list of two ints, representing the beginning and end of a range, [i, j]
     """
+    
     a = s.split()
     return [int(a[0]), int(a[1])]
 
 # ------------
-# Netflix_eval
+# netflix_eval
 # ------------
 
-def Netflix_eval (i, j) :
+def netflix_eval (movie_id, customer_id) :
     """
-    i the beginning of the range, inclusive
-    j the end       of the range, inclusive
-    return the max cycle length of the range [i, j]
+    movie_id the movie being rated
+    customer_id the customer rating being predicted for a particular movie
+    return the predicted value of the movie rating for the customer
     """
     # <your code>
     return 1
 
 # -------------
-# Netflix_print
+# netflix_print
 # -------------
 
-def Netflix_print (w, i, j, v) :
+def netflix_print (w, i) :
     """
-    print three ints
+    print one line of text
     w a writer
-    i the beginning of the range, inclusive
-    j the end       of the range, inclusive
-    v the max cycle length
+    i the movie_id or the predicted score
     """
-    w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
+    w.write(str(i) + "\n")
 
 # -------------
-# Netflix_solve
+# netflix_solve
 # -------------
 
-def Netflix_solve (r, w) :
+def netflix_solve (r, w) :
     """
     r a reader
     w a writer
     """
     for s in r :
-        i, j = Netflix_read(s)
-        v    = Netflix_eval(i, j)
-        Netflix_print(w, i, j, v)
+        i, j = netflix_read(s)
+        v    = netflix_eval(i, j)
+        netflix_print(w, i, j, v)
